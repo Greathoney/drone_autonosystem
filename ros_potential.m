d42e0f51634ea20a3268e3eee0b4eb06c3436b91
Greathoney <thyoon48@gmail.com>
2021-12-10 14:41:47 +0900

    dir fixed

--- ros_potential.m
+++ ros_potential.m
@@ -1,6 +1,11 @@
 close all; clear; clc
 
-addpath('catkin_ws/src/mavros/matlab_msg_gen_ros1/glnxa64/install/m')
+[tmp, name] = dos("echo $USER");
+name = erase(name,newline);
+catkin_dir = '/home/%s/catkin_ws/src/mavros/matlab_msg_gen_ros1/glnxa64/install/m';
+sprintf(catkin_dir, name)
+
+addpath(sprintf(catkin_dir, name))
 addpath('gui')
 % rosinit
 gui();
